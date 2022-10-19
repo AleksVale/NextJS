16893e97cda9a79298fd7b06f8941582418d441f
Aleksander <alexalexx3@gmail.com>
2022-10-19 15:39:56 -0300

    iniutial

---
+++ ignite-shop/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from "../styles"
+
+const button = styled('button', {
+  
+})
+
+export default function Home() {
+  return (
+   <h1>Hello world</h1>
+  )
+}
---
+++ ignite-shop/src/pages/sucess.tsx
@@ -0,0 +1,5 @@
+export default function Sucess() {
+  return(
+    <h1>MALOU PAPAI</h1>
+  )
+}
